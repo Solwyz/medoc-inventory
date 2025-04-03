129a7f6f8e9a3716daf7574df8ac7850e657a5e7
VineeshSolwyz <vineesh.web.solwyz@gmail.com>
2025-04-03 09:24:34 +0530

    UI bug fixing

--- src/Inventory/Pages/Orders/Orders.js
+++ src/Inventory/Pages/Orders/Orders.js
@@ -41,9 +41,9 @@ function Orders() {
 
 
   const orders = [
-    { id: 1, orderId: "652542", date: "17 Jan, 2025", items: "02", amount: "AED 240", payment: "COD", invoice: "CT/12/2544", status: "Complete" },
-    { id: 2, orderId: "652543", date: "18 Jan, 2025", items: "03", amount: "AED 360", payment: "Online", invoice: "CT/12/2545", status: "Ongoing" },
-    { id: 3, orderId: "652544", date: "19 Jan, 2025", items: "01", amount: "AED 120", payment: "Card", invoice: "CT/12/2546", status: "Pending" },
+    { id: 1, orderId: "652542", date: "17 Jan, 2025", products: "Face cream-120ml", items:"30", amount: "AED 240", payment: "COD",  expectedDelivey: "17 Jan, 2025", status: "Complete" },
+    { id: 2, orderId: "652543", date: "18 Jan, 2025", products: "Face cream-120ml", items:"30", amount: "AED 360", payment: "Paid",  expectedDelivey: "17 Jan, 2025", status: "Ongoing" },
+    { id: 3, orderId: "652544", date: "19 Jan, 2025", products: "Face cream-120ml", items:"30", amount: "AED 120", payment: "COD",  expectedDelivey: "17 Jan, 2025", status: "Pending" },
   ];
 
   const statusStyles = {
@@ -52,7 +52,7 @@ function Orders() {
     Pending: "text-[#FF0000] bg-[#FFE7E7]",
   };
 
-  const tabs = ["New orders", "Ongoing", "Dispatched", "Delivered", "Cancelled"];
+  const tabs = ["New orders", "Pending", "Ongoing", "Completed", "Cancelled", "Return"];
 
   const handleDeleteClick = (id) => {
     setOrderToDelete(id);
@@ -74,22 +74,22 @@ function Orders() {
         <div className="bg-white p-6 mt-8 rounded-lg h-full min-h-svh">
           <div className="flex justify-between">
             <div className="flex">
-              <div className="border rounded-lg items-center w-[584px] py-[12px] pl-4 text-[#696A70] flex">
+              <div className="border hover:border-[#8F8F8F] focus-within:border-[#8F8F8F] rounded-lg items-center w-[584px] py-[12px] pl-4 text-[#2C2B2B] flex">
                 <img className="mr-2 w-4 h-4" src={SearchIcon} alt="" />
                 <input type="text" placeholder="Search" className="outline-none w-full" />
               </div>
-              <div className="border rounded-lg w-[96px] py-[14px] pl-4 text-[#696A70] flex ml-4">
+              <div className="border hover:border-[#8F8F8F] focus-within:border-[#8F8F8F] rounded-lg w-[96px] py-[14px] pl-4 text-[#2C2B2B] flex ml-4 ">
                 <img className="mr-2" src={FilterIcon} alt="" />
-                <input type="text" placeholder="Filter" className="outline-none w-full" />
+                <input type="text" placeholder="Filter" className="outline-none w-full placeholder:text-[#2C2B2B]" />
               </div>
             </div>
-            <div className="border rounded-lg py-[14px] px-4 text-[#2C2B2B] flex ml-4">
+            <div className="border hover:border-[#8F8F8F]  rounded-lg py-[14px] px-4 text-[#2C2B2B] flex ml-4">
               <img className="mr-2" src={Export} alt="" />
               Export
             </div>
           </div>
 
-          <div className="bg-[#F0F0F0] px-1 py-1 w-fit mt-8 rounded-lg ">  
+          <div className="bg-[#F0F0F0] px-1 py-1 w-fit mt-8 rounded-lg ">
             <div className="flex">
               {tabs.map((tab) => (
                 <div
@@ -113,44 +113,34 @@ function Orders() {
               <table className="w-full bg-white mt-4 border">
                 <thead className="bg-[#F0F0F0] px-6 py-4">
                   <tr>
-                    <th className="p-4 font-medium text-center text-sm flex items-center">
-                      <img
-                        src={selectAllChecked ? checkedBox : checkBox}
-                        className="mr-4 cursor-pointer"
-                        alt="select all"
-                        onClick={toggleSelectAll}
-                      />
-                      Order ID
-                    </th>
+                    <th className="p-4 font-medium text-center text-sm flex items-center"> Order ID</th>
                     <th className="p-4 font-medium text-center text-sm">Date</th>
+                    <th className="p-4 font-medium text-center text-sm">Product</th>
                     <th className="p-4 font-medium text-center text-sm">Item</th>
-                    <th className="p-4 font-medium text-center text-sm">Amount</th>
                     <th className="p-4 font-medium text-center text-sm">Payment Mode</th>
-                    <th className="p-4 font-medium text-center text-sm">Invoice ID</th>
-                    <th className="p-4 font-medium text-center text-sm">Status</th>
-                    <th className="p-4 font-medium text-center text-sm">Action</th>
+                    <th className="p-4 font-medium text-center text-sm">Expected Delivery</th>
+                    <th className="p-4 font-medium text-center text-sm">Amount</th>
+                    {/* <th className="p-4 font-medium text-center text-sm">Action</th> */}
                   </tr>
                 </thead>
                 <tbody>
-                  {orders.map(({ id, orderId, date, items, amount, payment, invoice, status }) => (
+                  {orders.map(({ id, orderId, date, products, items,expectedDelivey, amount, payment, invoice, status }) => (
                     <tr key={id}>
-                      <td className="p-4 font-medium text-center text-sm flex items-center">
-                        <img src={checkedItems[id] ? checkedBox : checkBox} className="mr-4" alt=""
-                          onClick={() => toggleCheckbox(id)} />
-                        {orderId}</td>
+                      <td className="p-4 font-medium text-center text-sm flex items-center">{orderId}</td>
                       <td className="p-4 font-medium text-center text-sm">{date}</td>
+                      <td className="p-4 font-medium text-center text-sm">{products}</td>
                       <td className="p-4 font-medium text-center text-sm">{items}</td>
-                      <td className="p-4 font-medium text-center text-sm">{amount}</td>
                       <td className="p-4 font-medium text-center text-sm">{payment}</td>
-                      <td className="p-4 font-medium text-center text-sm">{invoice}</td>
-                      <td className="p-4 font-normal text-center text-[12px]">
+                      <td className="p-4 font-medium text-center text-sm">{expectedDelivey}</td>
+                      <td className="p-4 font-medium text-center text-sm">{amount}</td>
+                      {/* <td className="p-4 font-normal text-center text-[12px]">
                         <button className={`py-1 px-4 rounded-lg ${statusStyles[status]}`}>{status}</button>
-                      </td>
-                      <td className="p-4 font-medium text-center text-sm">
+                      </td> */}
+                      {/* <td className="p-4 font-medium text-center text-sm">
                         <button onClick={() => handleDeleteClick(id)}>
                           <img src={Delete} alt="delete" />
                         </button>
-                      </td>
+                      </td> */}
                     </tr>
                   ))}
                 </tbody>
@@ -177,5 +167,5 @@ function Orders() {
     </div>
   );
 }
- 
+
 export default Orders;
